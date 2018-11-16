f88af7977d46d3f1a6ceaf38f6ac05cff818697d
ljurk <ljurk@pm.me>
2018-11-16 17:46:45 +0000

    add colorful output, csv as input parameter

--- soap.py
+++ soap.py
@@ -1,42 +1,86 @@
-#refrence: https://python-zeep.readthedocs.io/en/master/api.html
+'''
+refrence: https://python-zeep.readthedocs.io/en/master/api.html
 
+prerequisites:
+	pip install zeep
+	pip install colorama
+'''
+import sys
 import csv
+from colorama import Fore, Style, init
+from zeep import Client
 
-#from zeep import Client
+#colors
+cRed = Style.BRIGHT + Fore.RED
+cDefault = Style.RESET_ALL
+cGreen = Style.BRIGHT + Fore.GREEN
+cYellow = Style.BRIGHT + Fore.YELLOW
 
+#variables
 list = []
 verbose = 0
 wsdl = 'SOME_WSDL_URL'
+csvSet = 0
+pathToCsv = ''
 
-#initialize
-print("initializing SOAP-Client")
-print("WSDL: " + wsdl)
-#soapClient = Client(wsdl)
+#init colorama
+init()
 
-#read csv
-with open('test.csv','r') as f:
-    print("start reading file")
-    reader = csv.DictReader(f,delimiter=';')
-    for row in reader:
-        list.append(row)
-    print("reading complete")
-    print("lines:" + str(len(list)))
-    print()
+#initialize SOAP Client
+print(cYellow + "initializing SOAP-Client")
+soapClient = Client(wsdl)
 
-#print csv data
-if verbose:
-    for i in list:
-        print(i)
-print('')
-print("start sending")
-for data in list:
+if len(sys.argv) != 1:
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == 'verbose':
+            verbose=i
+        elif '.csv' in sys.argv[i]: 
+            pathToCsv=sys.argv[i]
+            csvSet = 1
+
+if not csvSet:
+    print(cRed + "ERROR: no csv given")
+else:
+    print(cYellow + "WSDL: " + wsdl)
+    print(cYellow + "CSV: " + pathToCsv)
+    
     print('')
-    print(data['userName'])
-    try:
-        print("ok")
-         #result = soapClient.service.func(data)
-    except:
-        print("An exception occurred")
+    
+    #read csv
+    with open(pathToCsv,'r') as f:
+        print("start reading file")
+        reader = csv.DictReader(f, delimiter=';')
+        for row in reader:
+            list.append(row)
+    
+    print(cGreen + "reading complete")
+    print("Number of lines:" + str(len(list)))
+    
+    print('')
+    
+    #print csv data
+    if verbose:
+        for i in list:
+            print(i)
+    print('')
+    
+    #send data
+    print(cRed + "start sending")
+    for data in list:
+        print('')
+        print(cYellow + data['userName'])
+        try:
+            print(cGreen + "ok")
+            result = soapClient.service.func(data)
+            if result.status == 'error':
+                print(cRed + result.status.upper() +"    "+ result.message)
+            elif result.status == 'success':
+                print(cGreen + result.status.upper() +"    "+ result.message)
+            else:
+                print(cRed + "unknown status:" + result.status)
+        except:
+            print(cRed + "An exception occurred")
+    print('')
 print("END")
 
 
